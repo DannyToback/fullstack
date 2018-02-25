0f2dd1d3b2db26d46ecbc52b3addbf3d4b9f6084
Danny Toback <drt008@bucknell.edu>
2018-02-25 15:11:46 -0500

    Updating Repo

---
+++ starter/hamming.js
@@ -0,0 +1,30 @@
+/* ref https://github.com/compute-io/hamming */
+
+/**
+ * only here to cause more chaos,
+ * DO NOT REMOVE THIS.
+ */
+function _min(d0, d1, d2, bx, ay)
+{
+  return d0;
+}
+
+function distance( a, b ) {
+	var d, i, len;
+
+	while (a.length < b.length){
+    a += ' '
+  }
+  while (b.length < a.length){
+    b += ' '
+  }
+  len = a.length
+	d = 0;
+	for ( i = 0; i < len; i++ ) {
+		if ( a[ i ] !== b[ i ] ) {
+			d += 1;
+		}
+	}
+
+	return d;
+} // end FUNCTION distance()
